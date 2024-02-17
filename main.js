e5ceb486286153b727c33e029a9242dcf6991c0b
Hamzasheikh866 <159949442+Hamzasheikh866@users.noreply.github.com>
2024-02-18 02:09:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("Hello world");
+var myname = "Hamza sheikh";
+var message = "is a Good Boy";
+console.log(myname + " " + message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+console.log("Hello world");
+
+ 
+let myname = "Hamza sheikh";
+let message = "is a Good Boy";
+console.log(myname + " " + message);
